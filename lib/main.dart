c245531bdf658934f38f649b06594da8129259c2
Edwin Villa <villaceniceros@gmail.com>
2020-04-30 01:46:00 -0600

    Completed Angelas Rows and Columns  Challenge

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    MyApp()
-);
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
@@ -12,7 +8,36 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Container(
+                width: 100.0,
+                color: Colors.red,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    width: 100.0,
+                    height: 100.0,
+                    color: Colors.blue,
+                  ),
+                  Container(
+                    width: 100.0,
+                    height: 100.0,
+                    color: Colors.greenAccent,
+                  ),
+                ],
+              ),
+              Container(
+                width: 100.0,
+                color: Colors.lightGreen,
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
