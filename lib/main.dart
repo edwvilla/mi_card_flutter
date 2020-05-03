f3dacc9a5646c808d29fe1619f812119ce33f63f
Edwin Villa <villaceniceros@gmail.com>
2020-05-02 22:19:12 -0600

    Completed mi card challenge

--- lib/main.dart
+++ lib/main.dart
@@ -7,33 +7,63 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.blueGrey[900],
         body: SafeArea(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
-              Container(
-                width: 100.0,
-                color: Colors.red,
+              CircleAvatar(
+                radius: 100.0,
+                backgroundImage: AssetImage('images/profile.jpg'),
               ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Container(
-                    width: 100.0,
-                    height: 100.0,
-                    color: Colors.blue,
-                  ),
-                  Container(
-                    width: 100.0,
-                    height: 100.0,
-                    color: Colors.greenAccent,
-                  ),
-                ],
+              Text(
+                'Edwin Villa',
+                style: TextStyle(
+                  fontSize: 50.0,
+                  fontFamily: 'OleoScript',
+                  color: Colors.white,
+                ),
               ),
-              Container(
-                width: 100.0,
-                color: Colors.lightGreen,
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 17.0,
+                  color: Colors.white24,
+                  letterSpacing: 2.5,
+                ),
+              ),
+              Divider(),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                color: Colors.white,
+                child: ListTile(
+                  leading: Icon(Icons.call, size: 35.0, color: Colors.blueGrey),
+                  title: Text(
+                    '+52 667 3021737',
+                    style: TextStyle(
+                      fontSize: 22.0,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                color: Colors.white,
+                child: ListTile(
+                  leading: Icon(
+                    Icons.mail,
+                    size: 35.0,
+                    color: Colors.blueGrey,
+                  ),
+                  title: Text(
+                    'villaceniceros@gmail.com',
+                    style: TextStyle(
+                      fontSize: 22.0,
+                    ),
+                  ),
+                ),
               ),
             ],
           ),
